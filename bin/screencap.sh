5a51f07a3572413854f6c2abdeaba8af5750ec41
Brian Tomlinson <darthlukan@gmail.com>
2015-10-13 20:09:36 +0200

    use the correct preset file, fix broken syntax

--- bin/screencap.sh
+++ bin/screencap.sh
@@ -9,15 +9,19 @@ MONITOR1=0
 MONITOR2=1920
 MONITOR3=3840
 
-case $1 in:
+case $1 in
     1)
         X1=$MONITOR1
+        ;;
     2)
         X1=$MONITOR2
+        ;;
     3)
         X1=$MONITOR3
+        ;;
     *)
         X1=$MONITOR2
+        ;;
 esac
 
-ffmpeg -f x11grab -r 60 -s ${WIDTH}x${HEIGHT} -i ":0.0+$X1,0" -vcodec libx264 -vpre 1080p -threads 0 ${DIR}${2}.mkv
+ffmpeg -f x11grab -r 60 -s ${WIDTH}x${HEIGHT} -i ":0.0+$X1,0" -vcodec libx264 -vpre libvpx-1080p -threads 0 ${DIR}${2}.mkv
